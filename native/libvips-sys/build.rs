e77f2ac1cafdefe49145746deb9e9924770b8ae0
bongole <bongole2@gmail.com>
2021-03-19 19:29:19 +0900

    impl

---
+++ native/libvips-sys/build.rs
@@ -0,0 +1,34 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    let lib = pkg_config::Config::new()
+        .atleast_version("8.10")
+        .probe("vips")
+        .unwrap();
+
+    let bindings = {
+        let mut builder = bindgen::Builder::default()
+            .header("src/wrapper.h")
+            .rustified_enum("*")
+            .bitfield_enum("*Flags?")
+            .whitelist_function("vips_.*")
+            .whitelist_function("g_(setenv|object|signal|value|type).*")
+            .layout_tests(false)
+            .generate_comments(false);
+        
+        for path in lib.include_paths {
+            builder = builder.clang_arg(format!("-I{}", path.to_str().unwrap()));
+        }
+
+        builder
+    }
+    .generate()
+    .expect("Unable to generate bindings!");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
