ddf9db20fe2dce99acd630bc3e99455f2a1acddc
bongole <bongole2@gmail.com>
2021-03-29 19:44:45 +0900

    WIP

--- native/libvips/src/lib.rs
+++ native/libvips/src/lib.rs
@@ -4,13 +4,13 @@ use std::{
     ffi::{c_void, CStr, CString},
     mem::transmute,
     os::raw::c_char,
-    ptr::{null_mut, slice_from_raw_parts_mut},
+    ptr::{null_mut, slice_from_raw_parts, slice_from_raw_parts_mut},
     sync::Once,
 };
 
 pub struct VipsImage<'a> {
     pub(crate) vips_image: *mut libvips_sys::VipsImage,
-    pub(crate) vips_source: &'a VipsSourceCustom,
+    pub(crate) _vips_source: &'a VipsSourceCustom,
 }
 
 type ReadHandlerType = (Option<u64>, Option<Box<dyn FnMut(&mut [u8]) -> usize>>);
@@ -19,15 +19,24 @@ pub struct VipsSourceCustom {
     pub(crate) read_handler: ReadHandlerType,
 }
 
-#[derive(Debug)]
+type WriteHandlerType = (Option<u64>, Option<Box<dyn FnMut(&[u8]) -> usize>>);
+type FinishHandlerType = (Option<u64>, Option<Box<dyn FnMut()>>);
 pub struct VipsTargetCustom {
     pub(crate) vips_target_custom: *mut libvips_sys::VipsTargetCustom,
+    pub(crate) write_handler: WriteHandlerType,
+    pub(crate) finish_handler: FinishHandlerType,
 }
 
 impl Drop for VipsSourceCustom {
     fn drop(&mut self) {
         unsafe {
-            libvips_sys::g_object_unref(self.vips_source_custom as libvips_sys::gpointer);
+            let source = self.vips_source_custom as libvips_sys::gpointer;
+
+            if let Some(handler_id) = self.read_handler.0 {
+                libvips_sys::g_signal_handler_disconnect(source, handler_id);
+            }
+
+            libvips_sys::g_object_unref(source);
         }
     }
 }
@@ -35,6 +44,16 @@ impl Drop for VipsSourceCustom {
 impl Drop for VipsTargetCustom {
     fn drop(&mut self) {
         unsafe {
+            let target = self.vips_target_custom as libvips_sys::gpointer;
+
+            if let Some(handler_id) = self.write_handler.0 {
+                libvips_sys::g_signal_handler_disconnect(target, handler_id);
+            }
+
+            if let Some(handler_id) = self.finish_handler.0 {
+                libvips_sys::g_signal_handler_disconnect(target, handler_id);
+            }
+
             libvips_sys::g_object_unref(self.vips_target_custom as libvips_sys::gpointer);
         }
     }
@@ -82,6 +101,143 @@ impl VipsSourceCustom {
 
         self.read_handler = (Some(handler_id), Some(Box::new(f)));
     }
+}
+
+impl VipsTargetCustom {
+    pub fn set_on_write<F>(&mut self, f: F)
+    where
+        F: FnMut(&[u8]) -> usize,
+        F: 'static,
+    {
+        let handler_id = unsafe {
+            #[allow(non_snake_case)]
+            unsafe extern "C" fn write_wrapper(
+                _target: *mut libvips_sys::VipsTargetCustom,
+                buf: *mut c_void,
+                buf_len: libvips_sys::gint64,
+                data: *mut c_void,
+            ) -> usize {
+                let this: &mut VipsTargetCustom = &mut *(data as *mut VipsTargetCustom);
+                if let Some(ref mut callback) = this.write_handler.1 {
+                    let buf = slice_from_raw_parts(buf as *const u8, buf_len as usize);
+                    callback(&*buf)
+                } else {
+                    0
+                }
+            }
+
+            let write_k = CString::new("write").unwrap();
+            libvips_sys::g_signal_connect(
+                self.vips_target_custom as libvips_sys::gpointer,
+                write_k.as_ptr(),
+                Some(transmute(write_wrapper as *const fn())),
+                self as *mut _ as libvips_sys::gpointer,
+            )
+        };
+
+        self.write_handler = (Some(handler_id), Some(Box::new(f)));
+    }
+
+    pub fn set_on_finish<F>(&mut self, f: F)
+    where
+        F: FnMut(),
+        F: 'static,
+    {
+        let handler_id = unsafe {
+            #[allow(non_snake_case)]
+            unsafe extern "C" fn finish_wrapper(
+                _target: *mut libvips_sys::VipsTargetCustom,
+                data: *mut c_void,
+            ) {
+                let this: &mut VipsTargetCustom = &mut *(data as *mut VipsTargetCustom);
+                if let Some(ref mut callback) = this.finish_handler.1 {
+                    callback()
+                }
+            }
+
+            let finish_k = CString::new("finish").unwrap();
+            libvips_sys::g_signal_connect(
+                self.vips_target_custom as libvips_sys::gpointer,
+                finish_k.as_ptr(),
+                Some(transmute(finish_wrapper as *const fn())),
+                self as *mut _ as libvips_sys::gpointer,
+            )
+        };
+
+        self.finish_handler = (Some(handler_id), Some(Box::new(f)));
+    }
+
+    pub fn is_finished(&self) -> bool {
+        unsafe { 
+            let parent = (*self.vips_target_custom).parent_object;
+            parent.finished == 1
+        }
+    }
+}
+
+impl<'a> VipsImage<'a> {
+    pub fn write_to_target(&self, target: &VipsTargetCustom, suffix: &str) -> bool {
+        unsafe {
+            let suffix_k = CString::new(suffix).unwrap();
+            let r = libvips_sys::vips_image_write_to_target(
+                self.vips_image,
+                suffix_k.as_ptr(),
+                target.vips_target_custom as *mut libvips_sys::VipsTarget,
+                null_mut::<*const c_void>()
+            );
+
+            r == 0
+        }
+    }
+}
+
+pub fn new_source_custom() -> VipsSourceCustom {
+    let mut vsc = VipsSourceCustom {
+        vips_source_custom: null_mut(),
+        read_handler: (None, None),
+    };
+
+    unsafe {
+        let vips_source_ptr = libvips_sys::vips_source_custom_new();
+        vsc.vips_source_custom = vips_source_ptr;
+    }
+
+    vsc
+}
+
+pub fn new_image_from_source(source: &VipsSourceCustom) -> VipsImage {
+    let mut vi = VipsImage {
+        vips_image: null_mut(),
+        _vips_source: source,
+    };
+
+    unsafe {
+        let empty_str = CString::new("").unwrap();
+        let vips_image_ptr = libvips_sys::vips_image_new_from_source(
+            source.vips_source_custom as *mut libvips_sys::VipsSource,
+            empty_str.as_ptr(),
+            null_mut::<*const c_char>(),
+        );
+
+        vi.vips_image = vips_image_ptr;
+    }
+
+    vi
+}
+
+pub fn new_target_custom() -> VipsTargetCustom {
+    let mut vtc = VipsTargetCustom {
+        vips_target_custom: null_mut(),
+        write_handler: (None, None),
+        finish_handler: (None, None),
+    };
+
+    unsafe {
+        let vips_target_ptr = libvips_sys::vips_target_custom_new();
+        vtc.vips_target_custom = vips_target_ptr;
+    }
+
+    vtc
 }
 
 static INIT: Once = Once::new();
@@ -133,57 +289,10 @@ pub fn set_simd_enabled(b: bool) {
     unsafe { libvips_sys::vips_vector_set_enabled(bi) }
 }
 
-pub fn new_source_custom() -> VipsSourceCustom {
-    let mut vsc = VipsSourceCustom {
-        vips_source_custom: null_mut(),
-        read_handler: (None, None),
-    };
-
-    unsafe {
-        let vips_source_ptr = libvips_sys::vips_source_custom_new();
-        vsc.vips_source_custom = vips_source_ptr;
-    }
-
-    vsc
-}
-
-pub fn new_image_from_source(source: &VipsSourceCustom) -> VipsImage {
-    let mut vi = VipsImage {
-        vips_image: null_mut(),
-        vips_source: source,
-    };
-
-    unsafe {
-        let empty_str = CString::new("").unwrap();
-        let vips_image_ptr = libvips_sys::vips_image_new_from_source(
-            source.vips_source_custom as *mut libvips_sys::VipsSource,
-            empty_str.as_ptr(),
-            null_mut::<*const c_char>(),
-        );
-
-        vi.vips_image = vips_image_ptr;
-    }
-
-    vi
-}
-
-pub fn new_target_custom() -> VipsTargetCustom {
-    let mut vtc = VipsTargetCustom {
-        vips_target_custom: null_mut(),
-    };
-
-    unsafe {
-        let vips_target_ptr = libvips_sys::vips_target_custom_new();
-        vtc.vips_target_custom = vips_target_ptr;
-    }
-
-    vtc
-}
-
 pub fn error() -> String {
     unsafe {
         let s = libvips_sys::vips_error_buffer();
-        CStr::from_ptr(s).to_str().unwrap().to_string()
+        CStr::from_ptr(s).to_string_lossy().to_string()
     }
 }
 
@@ -201,7 +310,9 @@ pub fn concurrency() -> i32 {
 
 #[cfg(test)]
 mod tests {
-    use std::{fs::File, io::Read};
+    use std::{fs::{File, metadata}, io::{Read, Write}, os::unix::prelude::MetadataExt};
+
+    use tempfile::NamedTempFile;
 
     #[test]
     fn test_init() {
@@ -255,6 +366,35 @@ mod tests {
         src.set_on_read(move |buf| file.read(buf).unwrap());
 
         let vi = crate::new_image_from_source(&src);
+
         assert!(!vi.vips_image.is_null());
+    }
+
+    #[test]
+    fn test_target_custom() {
+        crate::init();
+        let mut src = crate::new_source_custom();
+        let mut target = crate::new_target_custom();
+
+        let file_path = format!("{}/test/test.jpg", env!("CARGO_MANIFEST_DIR"));
+        let mut file = File::open(file_path).unwrap();
+
+        src.set_on_read(move |buf| file.read(buf).unwrap());
+
+        let mut tmpfile = NamedTempFile::new().unwrap();
+        let tmpfile_path = tmpfile.path().to_str().unwrap().to_string();
+        target.set_on_write(move |buf| { 
+            tmpfile.write(buf).unwrap()
+        });
+
+        let vi = crate::new_image_from_source(&src);
+        let r = vi.write_to_target(&target, ".png");
+
+        let tmpfile_metadata = metadata(tmpfile_path).unwrap();
+
+        assert!(r);
+        assert!(!vi.vips_image.is_null());
+        assert!(target.is_finished());
+        assert!(0 < tmpfile_metadata.size());
     }
 }
