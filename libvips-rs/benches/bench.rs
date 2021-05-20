30758e2a08dc25854ab7af3c8423e7f6a29975e7
bongole <bongole2@gmail.com>
2021-05-20 14:19:45 +0900

    fix

--- libvips-rs/benches/bench.rs
+++ libvips-rs/benches/bench.rs
@@ -6,12 +6,6 @@ use std::{
 };
 
 fn resize(width: i32) {
-    libvips_rs::init();
-    //libvips_rs::leak_set(true);
-
-    libvips_rs::cache_set_max_mem(0);
-    libvips_rs::cache_set_max(0);
-
     let mut src = libvips_rs::new_source_custom();
     let mut target = libvips_rs::new_target_custom();
 
@@ -24,7 +18,7 @@ fn resize(width: i32) {
     target.set_on_write(move |buf| outfile.write(buf).unwrap() as i64);
 
     let vi = libvips_rs::thumbnail_from_source(src, width).unwrap();
-    vi.write_to_target(&target, ".jpg");
+    vi.write_to_target(&target, ".jpg").unwrap();
 
     libvips_rs::clear_error();
     libvips_rs::thread_shutdown();
@@ -32,7 +26,12 @@ fn resize(width: i32) {
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut g = c.benchmark_group("mygroup");
-    g.sample_size(30);
+
+    libvips_rs::init();
+    libvips_rs::cache_set_max_mem(0);
+    libvips_rs::cache_set_max(0);
+
+    g.sample_size(100);
     g.bench_function("resize", |b| b.iter(|| resize(black_box(1000))));
     g.finish();
 }
